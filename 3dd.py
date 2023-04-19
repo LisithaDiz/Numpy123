2f35bb6db6c5b27672750b55e1ec5e73c70549c2
Lisitha Dissanayaka <114801465+LisithaDiz@users.noreply.github.com>
2023-04-20 00:28:21 +0530

    Commit

--- 3dd.py
+++ 3dd.py
@@ -31,7 +31,7 @@ for _ in range(1000):
     x_new, y_new = currnt_pos[0] - L * X_der, currnt_pos[1] - L*Y_der
     currnt_pos = (x_new, y_new, z_func(x_new, y_new))
 
-    ax.plot_surface(X, Y, Z, cmap='viridis')
-    ax.scatter(currnt_pos[0], currnt_pos[1], currnt_pos[2], color='red', zorder=1)
+    ax.plot_surface(X, Y, Z, cmap='viridis', zorder=0)
+    ax.scatter(currnt_pos[0], currnt_pos[1], currnt_pos[2], color='magenta', zorder=1)
     plt.pause(0.001)
     ax.clear()
